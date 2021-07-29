80579b409462e933b4ee5fa24a3076ec8b5ced8c
NICE_SYSTEMS\ihort <Ihor.Telepenko@nice.com>
2021-07-29 13:33:22 +0300

    reading & receiving of array is configured

--- WinFormsApp/FormSortApp.cs
+++ WinFormsApp/FormSortApp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Deployment.Internal;
+using System.Diagnostics;
 using System.Drawing;
 using System.Dynamic;
 using System.IO;
@@ -17,7 +18,7 @@ namespace WinFormsApp
 {
     public partial class FormSortApp : Form
     {
-        public dynamic basicArray2D { get; set; }
+        public dynamic basicArray2D { get; set; }   // may be 2d array of different type
         public DataType ArrType { get; set; }
 
         public FormSortApp()
@@ -27,11 +28,10 @@ namespace WinFormsApp
 
         private void buttonArrReadingByPath_Click(object sender, EventArgs e)
         {
+            CleanVisibleArrayCharacteristics();
+
             var pathToFile = textBoxFilePath.Text;
-            //if (!File.Exists(pathToFile))
-                //return;
-
-
+            
             ArrType = HelpFunctionalityOfArrReading.TypeOfArray2DStoredInFile(pathToFile);
 
             switch (ArrType)
@@ -42,25 +42,27 @@ namespace WinFormsApp
                 case DataType.DoubleType:
                     basicArray2D = new ArraySetter<double>(pathToFile).array2D;
                     break;
-                default:
+                case DataType.StringType:
                     basicArray2D = new ArraySetter<string>(pathToFile).array2D;
                     break;
             }
 
-            if(basicArray2D==null)
-                PrintOutput(textBoxBasicArrOutput, null);
-            else
-                PrintOutput(textBoxBasicArrOutput,
-                    UIHelpFunctionality.Arr2dToStringMatrix(basicArray2D, "    "));
+            if (basicArray2D == null)
+            {
+                CleanBasicArrayField();
+                return;
+            }
 
-            CleanVisibleArrayCharacteristics();
+            PrintOutput(textBoxBasicArrOutput,
+                UIHelpFunctionality.Arr2dToStringMatrix(basicArray2D, "    "));
+
         }
 
         private void buttonRandomArrayAssign_Click(object sender, EventArgs e)
         {
-            if (comboBoxDataTypeOfArr.SelectedItem != null)
+            if (comboBoxDataTypeOfArr.SelectedIndex > 0)
             {
-                if (numUpDownRowsInArr.Value != 0 && numUpDownColumnsInArr.Value != 0)
+                if (numUpDownRowsInArr.Value > 0 && numUpDownColumnsInArr.Value > 0)
                 {
                     int arr2dLengthRows = Convert.ToInt32(numUpDownRowsInArr.Value);
                     int arr2dLengthColumns = Convert.ToInt32(numUpDownColumnsInArr.Value);
@@ -73,20 +75,20 @@ namespace WinFormsApp
                         case DataType.DoubleType:
                             basicArray2D = new ArraySetter<double>(arr2dLengthRows, arr2dLengthColumns).array2D;
                             break;
-                        default:
+                        case DataType.StringType:
                             basicArray2D = new ArraySetter<string>(arr2dLengthRows, arr2dLengthColumns).array2D;
                             break;
                     }
 
-                    
                     PrintOutput(textBoxBasicArrOutput,
                         UIHelpFunctionality.Arr2dToStringMatrix(basicArray2D, "   "));
-                    
 
+                    return;
                 }
             }
 
             CleanVisibleArrayCharacteristics();
+            CleanBasicArrayField();
         }
 
         private void comboBoxDataTypeOfArr_SelectedIndexChanged(object sender, EventArgs e)
@@ -108,6 +110,10 @@ namespace WinFormsApp
             numUpDownColumnsInArr.Value = 0;
         }
 
-        
+        private void CleanBasicArrayField()
+        {
+            PrintOutput(textBoxBasicArrOutput, null);
+        }
+
     }
 }
