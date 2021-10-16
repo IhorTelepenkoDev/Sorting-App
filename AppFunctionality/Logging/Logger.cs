798f5ef1650f30fef974c6e677513a29d6b1eeae
IgorTelepenko <Admin@ASER2>
2021-10-16 13:07:58 +0200

    "singleton" centralized logger for all the projects (with single config file)

--- AppFunctionality/Logging/Logger.cs
+++ AppFunctionality/Logging/Logger.cs
@@ -9,19 +9,28 @@ namespace AppFunctionality.Logging
 {
     public class Logger
     {
+        private static Logger instance; //Logger is "Singleton"
+
         private readonly log4net.ILog _log;
-        private const string configFileLocalPath = @"\Logging\log4net.config";
+        private const string configFileLocalPath = @"AppFunctionality\Logging\log4net.config";
         private const string loggerName = "log4netFileLogger";
 
-        public Logger()
+        private Logger()
         {
-            var baseProjectDirectory = Path.GetFullPath(@"..\..\");
-            var configFileDirectory = baseProjectDirectory + configFileLocalPath;
+            var baseSolutionDirectory = Path.GetFullPath(@"..\..\..\");
+            var configFileDirectory = baseSolutionDirectory + configFileLocalPath;
 
             FileInfo configFileInfo = new FileInfo( configFileDirectory);
             log4net.Config.XmlConfigurator.ConfigureAndWatch(configFileInfo);
 
             _log = log4net.LogManager.GetLogger(loggerName);
+        }
+
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+                instance = new Logger();
+            return instance;
         }
 
         public void Debug(string value)
